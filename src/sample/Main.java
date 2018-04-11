5f5014882380da98f6ee9ce90566de5b9b86f32e
Martin <mackan_4000@hotmail.com>
2018-04-11 18:18:38 +0200

    added main

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -10,7 +10,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("LoginSample.fxml"));
         primaryStage.setTitle("Goodbye all");
         System.out.println("Testprint");
         primaryStage.setScene(new Scene(root));
