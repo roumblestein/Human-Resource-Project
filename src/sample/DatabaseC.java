9bd332247b1000ff9fa48614b91e3b55fedbdbd7
Martin <mackan_4000@hotmail.com>
2018-04-23 21:15:05 +0200

    big part of forgotten password done

--- src/sample/DatabaseC.java
+++ src/sample/DatabaseC.java
@@ -1,12 +1,35 @@
 package sample;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
 import java.sql.*;
 
 public class DatabaseC {
     private static DatabaseC instance = null;
-    protected DatabaseC() throws SQLException {
+    protected DatabaseC() throws SQLException {}
 
-    }
+
+    // ------------------Password FXML----------------------
+    @FXML
+    private TextField ssnText;
+    @FXML
+    private TextField passwordText;
+
+    private String SSN;
+
+
+    //--------------------USER-----------------------------
+
+    //-------------------Login-----------------------------
+
+
+
+
+
+
+
+
     public static DatabaseC getInstance () throws SQLException {
         if (instance == null) {
             instance = new DatabaseC();
@@ -22,4 +45,55 @@ public class DatabaseC {
 
         boolean rs = st.execute("INSERT INTO skills (skills.Skill, skills.Level, skills.Skillcategory) VALUES ('Programming', 'Newbie', 'development')");
     }
+
+
+
+    //--------------------------LOGIN METHODS----------------------------
+
+
+
+    //----------------------------USER METHODS-----------------------------
+
+
+    //-------------------------PASSWORD METHODS-----------------------------
+
+
+    public boolean CheckUsername (String user) throws SQLException {
+
+         PreparedStatement statement = c.prepareStatement("SELECT SSN from userlogin where SSN = '"+user+"'");
+        int username = 0;
+            ResultSet rs = statement.executeQuery();
+            while (rs.next()){
+                username = rs.getInt(1);
+            }
+            if (String.valueOf(username).equals(user) ){
+                SSN = user;
+                return true;
+            }else {
+                return false;
+            }
+
+    }
+
+    public void newPassword (String pass) throws SQLException {
+        PreparedStatement ps = c.prepareStatement("UPDATE userlogin SET Password = ? WHERE SSN = ?");
+        ps.setString(1,pass);
+        ps.setString(2, SSN);
+        ps.executeUpdate();
+        System.out.println("funka");
+        System.out.println(SSN +"  "+pass);
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
--- src/sample/forgotPassword.java
+++ src/sample/forgotPassword.java
@@ -1,19 +1,46 @@
 package sample;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 import java.net.URL;
+import java.security.SecureRandom;
+import java.sql.SQLException;
 import java.util.ResourceBundle;
 
 public class forgotPassword implements Initializable {
 
+
+    @FXML
+    private Button emailButton;
+
+    @FXML
+    private TextField emailCode;
+
+    @FXML
+    private TextField randomNR;
+
+    @FXML
+    private TextField SsnText;
+
+    @FXML
+    private TextField newPass;
+
+    @FXML
+    private TextField verifyPass;
+
+
+    int randomNumber;
+    String SSN;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
@@ -33,8 +60,62 @@ public class forgotPassword implements Initializable {
         stage.setScene(scene);
     }
 
+    public void emailButton (ActionEvent event) throws IOException, SQLException {
+
+        boolean reset = DatabaseC.getInstance().CheckUsername(SsnText.getText());
+
+        if (reset){
+            SsnText.setEditable(false);
+            emailButton.setVisible(false);
+            SecureRandom rand = new SecureRandom();
+            randomNumber = rand.nextInt(9000)+1000;
+            randomNR.setText(""+randomNumber);
+            emailCode.setDisable(false);
+            emailCode.setEditable(true);
+
+        }else {
+            //// error handling
+        }
+
+    }
+
+    public void newPassButton (ActionEvent event) throws IOException {
+
+        if (Integer.parseInt(emailCode.getText()) == randomNumber){
+
+            Node node = (Node)event.getSource();
+            Stage stage = (Stage) node.getScene().getWindow();
+
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("NewPass.fxml"));
+            Parent root = fxmlLoader.load();
+
+            Scene scene = new Scene(root);
+            stage.setScene(scene);
+        }
+
+    }
+    public void createNewPass (ActionEvent event) throws SQLException, IOException {
+
+        if (newPass.getText().equals(verifyPass.getText())){
+
+            DatabaseC.getInstance().newPassword(newPass.getText() );
+
+            Node node = (Node)event.getSource();
+            Stage stage = (Stage) node.getScene().getWindow();
+
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("LoginSample.fxml"));
+            Parent root = fxmlLoader.load();
+
+            Scene scene = new Scene(root);
+            stage.setScene(scene);
+
+        }
 
 
+
+
+
+    }
 
 
 
