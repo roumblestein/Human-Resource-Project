eb54086ab5b5c7061953ae769bc68583d2fd94f3
Felix Carlsson <felixcarlsson93@gmail.com>
2018-05-01 17:28:21 +0200

    Login update

--- src/sample/Login.java
+++ src/sample/Login.java
@@ -8,15 +8,18 @@ import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Alert;
-import javafx.scene.control.PasswordField;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 import javax.swing.*;
 import java.io.IOException;
 import java.net.URL;
+import java.util.Optional;
 import java.util.ResourceBundle;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Login implements Initializable {
 
@@ -25,56 +28,49 @@ public class Login implements Initializable {
     @FXML
     private PasswordField PasswordText;
 
+    @FXML
+    private Label ssnw;
+
+
+
+    //Scanner input = new Scanner(System.in);
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+
 
     }
 
     @FXML
     public void loginButton(ActionEvent event) throws IOException {
-            int x;
-            try{
-                x = Integer.parseInt(SsnText.getText());
-            }catch(NumberFormatException a){
-                Alert dialog1 = new Alert(Alert.AlertType.INFORMATION);
-                dialog1.setTitle("Information");
-                dialog1.setHeaderText("Information");
-                dialog1.setContentText("Use only numbers");
-                dialog1.show();
-            }
-            if (SsnText.getLength() != 10) {
+        /*String test = "111111-2222";
+        String[] tokens = test.split("-");
 
-                    Alert dialog1 = new Alert(Alert.AlertType.INFORMATION);
-                    dialog1.setTitle("Information");
-                    dialog1.setHeaderText("Information");
-                    dialog1.setContentText("Enter your 10 digit SSN");
-                    dialog1.show();
+       //private static final String USERNAME_REGEX = "^[0-9-]{11}$";
+        String Ssn = SsnText.getText();
+        String Password = PasswordText.getText();*/
 
+        if (SsnText.getLength() != 11) {
+            ssnw.setText("Write in this format YYMMDD-XXXX");
 
+        }else if (SsnText.getText().isEmpty() || PasswordText.getText().isEmpty()) {
+            ssnw.setText("Enter details in empty fields!");
+        }else {
+            Node node = (Node) event.getSource();
+            Stage stage = (Stage) node.getScene().getWindow();
 
-            } else if (SsnText.getText().isEmpty() || PasswordText.getText().isEmpty()) {
+            FXMLLoader FxmlLoader = new FXMLLoader(getClass().getResource("UserScreen.fxml"));
+            Parent root = FxmlLoader.load();
 
-                Alert dialog = new Alert(Alert.AlertType.INFORMATION);
-                dialog.setTitle("Information");
-                dialog.setHeaderText("Information");
-                dialog.setContentText("Enter details in empty fields");
-                dialog.show();
+            Scene scene = new Scene(root);
+            stage.setScene(scene);
 
-            }else {
-                Node node = (Node) event.getSource();
-                Stage stage = (Stage) node.getScene().getWindow();
-
-                FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("UserScreen.fxml"));
-                Parent root = fxmlLoader.load();
-
-                Scene scene = new Scene(root);
-                stage.setScene(scene);
-            }
+        }
     }
 
-    public void forgotButton (ActionEvent event) throws IOException {
+    public void forgotButton(ActionEvent event) throws IOException {
 
-        Node node = (Node)event.getSource();
+        Node node = (Node) event.getSource();
         Stage stage = (Stage) node.getScene().getWindow();
 
         FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("forgotPass.fxml"));
@@ -82,14 +78,18 @@ public class Login implements Initializable {
 
         Scene scene = new Scene(root);
         stage.setScene(scene);
+
+    }    @FXML
+    private void exitButton(ActionEvent event) {
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+        alert.setTitle("Exit");
+        alert.setHeaderText("Look, a Confirmation Dialog");
+        alert.setContentText("Do you want to exit ?");
+
+        Optional<ButtonType> result = alert.showAndWait();
+        if (result.get() == ButtonType.OK) {
+            System.exit(1);
+        }
     }
-
-
-
-
-
-
-
-
 
 }
